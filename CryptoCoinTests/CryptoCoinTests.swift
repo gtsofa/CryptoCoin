709630d700bbc7d2fe47fbac281adf3a7156288a
gtsofa <jlsnyule@yahoo.com>
2025-04-29 23:27:19 +0300

    Initial commit

---
+++ CryptoCoinTests/CryptoCoinTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinTests.swift
+//  CryptoCoinTests
+//
+//  Created by Julius on 29/04/2025.
+//
+
+import Testing
+@testable import CryptoCoin
+
+struct CryptoCoinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
