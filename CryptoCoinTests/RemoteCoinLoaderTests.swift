165d0d852ce31e3e9833ae30539ebe88a66999dd
gtsofa <jlsnyule@yahoo.com>
2025-04-30 15:31:39 +0300

    improve test readability

--- CryptoCoinTests/RemoteCoinLoaderTests.swift
+++ CryptoCoinTests/RemoteCoinLoaderTests.swift
@@ -46,7 +46,7 @@ final class RemoteCoinLoaderTests: XCTestCase {
         sut.load { capturedErrors.append($0)}
         
         let clientError = NSError(domain: "test", code: 0)
-        client.completions[0](clientError)
+        client.complete(with: clientError)
         
         XCTAssertEqual(capturedErrors, [.connectivity])
         
@@ -69,6 +69,10 @@ final class RemoteCoinLoaderTests: XCTestCase {
         func get(from url: URL, completion: @escaping (Error) -> Void) {
             requestedURLs.append(url)
             completions.append(completion)
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            completions[index](error)
         }
     }
 
