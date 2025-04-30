3061130da736df057464761775e52259848a5720
gtsofa <jlsnyule@yahoo.com>
2025-04-30 12:12:05 +0300

    Remove the Singleton implementation to use dependency injection

--- CryptoCoinTests/RemoteCoinLoaderTests.swift
+++ CryptoCoinTests/RemoteCoinLoaderTests.swift
@@ -7,23 +7,27 @@
 
 import XCTest
 
-class HTTPClient {
-    static var shared = HTTPClient()
-    
-    func get(from url: URL) {}
+protocol HTTPClient {
+    func get(from url: URL)
 }
 
 class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
     
-    override func get(from url: URL) {
+    func get(from url: URL) {
         requestedURL = url
     }
 }
 
 class RemoteCoinLoader {
+    private let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
     func load() {
-        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: URL(string: "https://a-url.com")!)
     }
 }
 
@@ -32,9 +36,8 @@ final class RemoteCoinLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        
-        _ = RemoteCoinLoader()
+
+        _ = RemoteCoinLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
         
@@ -42,9 +45,8 @@ final class RemoteCoinLoaderTests: XCTestCase {
     
     func test_load_requestsDataFromURL() {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
         
-        let sut = RemoteCoinLoader()
+        let sut = RemoteCoinLoader(client: client)
         
         sut.load()
         
