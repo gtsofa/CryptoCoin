141221655db1f181a0b521ae3fc830a571140114
gtsofa <jlsnyule@yahoo.com>
2025-04-30 17:01:25 +0300

    Fix a typo

--- CryptoCoinTests/RemoteCoinLoaderTests.swift
+++ CryptoCoinTests/RemoteCoinLoaderTests.swift
@@ -55,9 +55,6 @@ final class RemoteCoinLoaderTests: XCTestCase {
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
         
-        
-        
-      
         let samples = [199, 201, 300, 400, 500]
         
         samples.enumerated().forEach { index, code in
