83b79858ececb139c0c8980df0405a98e9b954a2
gtsofa <jlsnyule@yahoo.com>
2025-04-30 12:25:56 +0300

    Refactor to inject the URL in the RemoteCoinLoader

--- CryptoCoinTests/RemoteCoinLoaderTests.swift
+++ CryptoCoinTests/RemoteCoinLoaderTests.swift
@@ -21,13 +21,15 @@ class HTTPClientSpy: HTTPClient {
 
 class RemoteCoinLoader {
     private let client: HTTPClient
+    private let url: URL
     
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
     
     func load() {
-        client.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: url)
     }
 }
 
@@ -36,8 +38,9 @@ final class RemoteCoinLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
+        let url = URL(string: "https://-another-url.com")!
 
-        _ = RemoteCoinLoader(client: client)
+        _ = RemoteCoinLoader(url: url, client: client)
         
         XCTAssertNil(client.requestedURL)
         
@@ -45,8 +48,9 @@ final class RemoteCoinLoaderTests: XCTestCase {
     
     func test_load_requestsDataFromURL() {
         let client = HTTPClientSpy()
+        let url = URL(string: "https://a-given-url.com")!
         
-        let sut = RemoteCoinLoader(client: client)
+        let sut = RemoteCoinLoader(url: url, client: client)
         
         sut.load()
         
