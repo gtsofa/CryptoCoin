514c3791251b721a63c25c9ba59c5e3a89588dd4
gtsofa <jlsnyule@yahoo.com>
2025-04-30 13:38:26 +0300

    Move the RemoteCoinLoader to production

--- CryptoCoin/Coin API/RemoteCoinLoader.swift
+++ CryptoCoin/Coin API/RemoteCoinLoader.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public class RemoteCoinLoader {
+    private let client: HTTPClient
+    private let url: URL
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
--- CryptoCoinTests/RemoteCoinLoaderTests.swift
+++ CryptoCoinTests/RemoteCoinLoaderTests.swift
@@ -6,30 +6,13 @@
 //
 
 import XCTest
-
-protocol HTTPClient {
-    func get(from url: URL)
-}
+import CryptoCoin
 
 class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
     
     func get(from url: URL) {
         requestedURL = url
-    }
-}
-
-class RemoteCoinLoader {
-    private let client: HTTPClient
-    private let url: URL
-    
-    init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    func load() {
-        client.get(from: url)
     }
 }
 
