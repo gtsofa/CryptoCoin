3436c578cd0495f9978d85f52eece108dd005057
gtsofa <jlsnyule@yahoo.com>
2025-04-30 11:46:38 +0300

    System request data from URL on `load()` invocation

--- CryptoCoinTests/RemoteCoinLoaderTests.swift
+++ CryptoCoinTests/RemoteCoinLoaderTests.swift
@@ -7,29 +7,38 @@
 
 import XCTest
 
+class HTTPClient {
+    static var shared = HTTPClient()
+    
+    private init() {}
+    
+    var requestedURL: URL?
+}
+
+class RemoteCoinLoader {
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")!
+    }
+}
+
+
 final class RemoteCoinLoaderTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient.shared
+        _ = RemoteCoinLoader()
+        
+        XCTAssertNil(client.requestedURL)
+        
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_load_requestsDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteCoinLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 
 }
