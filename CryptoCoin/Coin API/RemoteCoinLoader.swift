23642a3c3fec8c26e7f9a1acf78318f9acd0a760
gtsofa <jlsnyule@yahoo.com>
2025-05-01 09:36:25 +0300

    Use explicit JSON data for non 200 HTTP response tests

--- CryptoCoin/Coin API/RemoteCoinLoader.swift
+++ CryptoCoin/Coin API/RemoteCoinLoader.swift
@@ -38,8 +38,8 @@ public class RemoteCoinLoader {
     public func load(completion: @escaping (Result) -> Void = { _ in }) {
         client.get(from: url) { result in
             switch result {
-            case let .success(data, _):
-                if let root = try? JSONDecoder().decode(Root.self, from: data) {
+            case let .success(data, response):
+                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
                     completion(.success(root.coins))
                 } else {
                     completion(.failure(.invalidData))
