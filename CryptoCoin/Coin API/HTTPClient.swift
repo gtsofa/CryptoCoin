6a79ede748365d9e7195136c0b147554d942ec4f
gtsofa <jlsnyule@yahoo.com>
2025-05-01 11:05:31 +0300

    Move HTTPClient to its own file

---
+++ CryptoCoin/Coin API/HTTPClient.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPClient.swift
+//  CryptoCoin
+//
+//  Created by Julius on 01/05/2025.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
