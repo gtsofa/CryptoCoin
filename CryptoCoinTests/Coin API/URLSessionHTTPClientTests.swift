e3853cb610f3695fbad9b3cba65973a69191eadc
gtsofa <jlsnyule@yahoo.com>
2025-05-01 18:35:31 +0300

    Creates URLSession data task with URL on URLSessionHTTPClient.get(from: URL)

---
+++ CryptoCoinTests/Coin API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,46 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  CryptoCoinTests
+//
+//  Created by Julius on 01/05/2025.
+//
+
+import XCTest
+
+class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) { _, _, _ in }
+    }
+}
+final class URLSessionHTTPClientTests: XCTestCase {
+    func test_getFromURL_createsDataTaskWithURL() {
+        
+        let url = URL(string: "https://a-url.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+    
+    // MARK: Helpers
+    
+    private class URLSessionSpy: URLSession {
+        var receivedURLs = [URL]()
+        
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> FakeURLSessionDataTask {
+            receivedURLs.append(url)
+            return FakeURLSessionDataTask()
+        }
+    }
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask { }
+
+}
