852fa00fd8fa6b1a0c42748e1541ea7fd89daa59
gtsofa <jlsnyule@yahoo.com>
2025-05-01 11:29:09 +0300

    Add memory leak detection

--- CryptoCoinTests/RemoteCoinLoaderTests.swift
+++ CryptoCoinTests/RemoteCoinLoaderTests.swift
@@ -103,11 +103,20 @@ final class RemoteCoinLoaderTests: XCTestCase {
     
     // MARK: Helpers
     
-    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteCoinLoader, client: HTTPClientSpy) {
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteCoinLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteCoinLoader(url: url, client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line )
+        
         return (sut, client)
         
+    }
+    
+    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
     }
     
     private func makeItemsJSON(_ coins: [[String: Any]]) -> Data {
