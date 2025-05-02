1b9a6042d13c58fb8a18d4292429cb46593981e9
gtsofa <jlsnyule@yahoo.com>
2025-05-02 09:55:24 +0300

    Extract pull to refresh simulation into a reusable extension on UIRefreshControl

--- CryptoCoinTests/CoinViewControllerTests.swift
+++ CryptoCoinTests/CoinViewControllerTests.swift
@@ -53,11 +53,7 @@ final class CoinViewControllerTests: XCTestCase {
         
         sut.loadViewIfNeeded()
         
-        sut.refreshControl?.allTargets.forEach { target in
-            sut.refreshControl?.actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
-                (target as NSObject).perform(Selector($0))
-            }
-        }
+        sut.refreshControl?.simulatePullToRefresh()
         
         XCTAssertEqual(loader.loadCallCount, 2)
     }
@@ -80,4 +76,14 @@ final class CoinViewControllerTests: XCTestCase {
         }
     }
 
+}
+
+private extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
 }
