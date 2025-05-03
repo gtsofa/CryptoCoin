8014a0317e238e375b40e0ab524fdf3ad8d7d5ae
gtsofa <jlsnyule@yahoo.com>
2025-05-03 14:30:02 +0300

    add Search bar

--- Prototype/Prototype/CoinViewController.swift
+++ Prototype/Prototype/CoinViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class CoinViewController: UITableViewController {
+    // Search controller
+    let searchController = UISearchController(searchResultsController: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(CryptoCoinCell.self, forCellReuseIdentifier: "CryptoCoinCell")
         tableView.rowHeight = UITableView.automaticDimension
         //tableView.estimatedRowHeight = 60
         tableView.separatorStyle = .none
+        
+        configureSearchController()
+    }
+    
+    func configureSearchController() {
+        // Configure search controller
+        //searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search by price or performance"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
+        // Optional: Add scope buttons for filtering by price or performance
+        searchController.searchBar.scopeButtonTitles = ["Price", "Performance"]
+        //searchController.searchBar.delegate = self
+        
+        // Initialize filtered items
+        //filteredItems = items
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
