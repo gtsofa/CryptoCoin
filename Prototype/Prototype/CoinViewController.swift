76fa607db072be49000384dec53c53929e0e4579
gtsofa <jlsnyule@yahoo.com>
2025-05-03 16:11:38 +0300

    Add prototype demo data

--- Prototype/Prototype/CoinViewController.swift
+++ Prototype/Prototype/CoinViewController.swift
@@ -7,7 +7,15 @@
 
 import UIKit
 
+struct CryptoCoinViewModel {
+    public let name: String
+    public let iconName: String
+    public let price: Double
+    public let dayPerformance: Double
+}
+
 class CoinViewController: UITableViewController {
+    private let coin = CryptoCoinViewModel.prototypeData
     // Search controller
     let searchController = UISearchController(searchResultsController: nil)
     
@@ -39,16 +47,24 @@ class CoinViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return coin.count //10
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CryptoCoinCell", for: indexPath) as! CryptoCoinCell
-        cell.symbolLabel.text = "BTC"
-        cell.nameLabel.text = "Bitcoin"
-        cell.priceLabel.text = "$50,000"
-        cell.dayPerformanceLabel.text = "-5.34"
-        //cell.iconImage.image = UIImage(named: "bitcoinIcon") // Set your image
+        let model = coin[indexPath.row]
+        cell.configure(with: model)
+        
         return cell
+    }
+}
+
+extension CryptoCoinCell {
+    func configure(with model: CryptoCoinViewModel) {
+        nameLabel.text = model.name
+        iconImage.image = UIImage(named: model.iconName)
+        priceLabel.text = String(format: "%.2f", model.price)
+        dayPerformanceLabel.text = String(format: "%.2f", model.dayPerformance)
+        
     }
 }
---
+++ Prototype/Prototype/CryptoCoinViewModel+PrototypeData.swift
@@ -0,0 +1,66 @@
+//
+//  CryptoCoinViewModel+PrototypeData.swift
+//  Prototype
+//
+//  Created by Julius on 03/05/2025.
+//
+
+import Foundation
+
+extension CryptoCoinViewModel {
+    static var prototypeData: [CryptoCoinViewModel] {
+        return [
+            CryptoCoinViewModel(
+                name: "Bitcoin",
+                iconName: "image-0",
+                price: 95985.55,
+                dayPerformance: -1.01),
+            CryptoCoinViewModel(
+                name: "Ethereum",
+                iconName: "image-1",
+                price: 1824.1917630869527,
+                dayPerformance: -0.45),
+            CryptoCoinViewModel(
+                name: "Tether USD",
+                iconName: "image-2",
+                price: 1.0004835358743913,
+                dayPerformance: 0.04),
+            CryptoCoinViewModel(
+                name: "XRP",
+                iconName: "image-3",
+                price: 2.193599713081481,
+                dayPerformance: -1.26),
+            CryptoCoinViewModel(
+                name: "BNB",
+                iconName: "image-4",
+                price: 596.8732933664612,
+                dayPerformance: -0.50),
+            CryptoCoinViewModel(
+                name: "Solana",
+                iconName: "image-5",
+                price: 147.23935015508818,
+                dayPerformance: -2.25),
+            CryptoCoinViewModel(
+                name: "USDC",
+                iconName: "image-6",
+                price: 1.0003134635070554,
+                dayPerformance: 0.06),
+            CryptoCoinViewModel(
+                name: "Dogecoin",
+                iconName: "image-7",
+                price: 0.1783158229265628,
+                dayPerformance: -1.64),
+            CryptoCoinViewModel(
+                name: "Cardano",
+                iconName: "image-8",
+                price: 0.6962331452340652,
+                dayPerformance: -1.64),
+            CryptoCoinViewModel(
+                name: "TRON",
+                iconName: "image-9",
+                price: 0.24806907457736618,
+                dayPerformance: 1.23)
+            
+        ]
+    }
+}
